2d20ecbd2fdd81cd7a44e4b02de4aae2d4b12d6b
Alexander Kladov <alexander.a.kladov@gmail.com>
2023-05-16 23:45:52 +0400

    First good upload to Alib

--- alib.js
+++ alib.js
@@ -1,8 +1,12 @@
 // ИНИЦИАЛИЗАЦИЯ - ДВОЙНОЙ ЩЕЛЧОК ДЛЯ РЕДАКТИРОВАНИЯ ЯЧЕЙКИ
-const cols = 17
+const cols = 18
 isbn_list = []
 isbn_index = 0
 alib_win = null;
+chitai_gorod_win = null;
+ozon_win = null;
+pic1 = null;
+pic2 = null;
 window.addEventListener("DOMContentLoaded", () => {
 for (let cell of document.querySelectorAll("#books td")) {
 cell.ondblclick = () => { editable.edit(cell); };
@@ -75,20 +79,33 @@ function readInputData() {
 
 function addFilledLine() {
      let obj = {
-                rubric:getListValue("rubrics-id","Рубрика"),
+                rubric:document.getElementById('tipfind-id').value,
                 authors:document.getElementById('authors-id').value,
                 name:document.getElementById('name-id').value,
                 publisher:document.getElementById('publisher-id').value,
                 date:document.getElementById('date-id').value,
                 pages:document.getElementById('pages-id').value,
                 description:document.getElementById('description-id').value,
-                binding:getListValue("bindings-id","Переплет"),
+                binding:getListValue("bindings-id","Переплет").split(' ')[0],
                 condition:getListValue("conditions-id","Состояние"),
-                format:getListValue("formats-id","Формат"),
+                format:getListValue("formats-id","Формат").split(' ')[0],
                 isbn:document.getElementById('isbn-id').value};
+                pic_str = ((pic1)?`${pic1}:Обложка:`:"")+((pic2)?`${pic2}:Задняя обл.:`:"");
                 new_date = new Date();
-                new_date = +new_date.getDate()+'.'+new_date.getMonth()+'.'+new_date.getFullYear();
-                addLine([obj.rubric, obj.authors, obj.name, '', '', obj.publisher, obj.date, obj.pages, obj.binding, obj.format, '', obj.description, obj.condition, new_date, '', '', isbn]);
+                day = `${new_date.getDate()}`;
+                month = `${new_date.getMonth()+1}`;
+                if ((obj.pages.length>0)&&(Number(obj.pages)>0)) {
+                    obj.pages=`${obj.pages} с.`;
+                } else {
+                    obj.pages='';
+                }
+                if (obj.rubric.length>0) {
+                    obj.rubric=`${obj.rubric}.`
+                }
+                isbn_str = `${isbn}`
+                isbn_str = isbn_str.slice(0,3)+'-'+isbn_str.slice(3,4)+'-'+isbn_str.slice(4,6)+'-'+isbn_str.slice(6,12)+'-'+isbn_str.slice(12,13);
+                new_date = ((day.length==2)?day : `0${day}`)+'.'+((month.length==2)?month : `0${month}`)+'.'+new_date.getFullYear();
+                addLine([obj.rubric, obj.authors, obj.name, '', '', obj.publisher, obj.date, obj.pages, obj.binding, obj.format, '', obj.description, obj.condition, new_date, pic_str, '', isbn_str]);
 }
 
 function addLine(list = null) {
@@ -98,7 +115,7 @@ function addLine(list = null) {
   	if (list) {
   		td.innerHTML = list[i];
   	} else {
-  		td.innerHTML = "abc";
+  		td.innerHTML = "";
   	}
   	td.ondblclick = () => { editable.edit(td); };
   	line.appendChild(td);
@@ -109,20 +126,36 @@ function addLine(list = null) {
 }
 
 function saveBooks() {
+   let error = false;
+   let errors=0;
+   let count_books=0;
    let books="";
    let count=0;
    for (let cell of document.querySelectorAll("#books td")) {
-   	if (count < cols) {
-   	    books += cell.innerHTML;
+    if (count < cols) {
+        line = cell.innerHTML.replaceAll("\n"," ").replaceAll("\t"," ").replaceAll('"','').replaceAll('«',' ')
+                            .replaceAll('»',' ').replaceAll('\'',' ').replaceAll(' .','. ').replaceAll(' ,',', ')
+                            .replaceAll(' ;','; ').replaceAll(' :',': ').replaceAll(' !','! ').replaceAll('<br>',' ').replaceAll('  ',' ').trim();
+   	    books += line;
+        if ((line.length==0) && (count == 0 || count == 2 || count == 11)) {
+            error = true;
+        }
+
    	    if (count < cols -1)
    	    	books += "\t";
    	    count += 1;
    	    if (count == cols) {
    	    	count = 0;
    	    	books += "\n";
+            if (error) {
+                error = false;
+                errors += 1;
+            }
+            count_books += 1;
    	    }
    	}
    }
+    alert(`Книг обработано ${count_books}, ошибок ${errors} (не заданы Рубрика, Название или Цена)`);
     let file = new File([books], "test_save.txt", {type: "text/plain"});
     let link = document.createElement('a');
     link.download = file.name;
@@ -159,6 +192,7 @@ function readISBNFile(files) {
 
   reader.onload = function() {
     isbns = reader.result.split('\n');
+    isbn_list=[];
     for (let i=0;i<isbns.length;i++) {
         if (isbns[i].length) {
             isbn_list.push(isbns[i]);
@@ -179,10 +213,24 @@ function findOnAlib() {
     alib_win = window.open(`https://www.alib.ru/findp.php4?isbnp=${isbn}`);
 }
 
+function findOnChitaiGorod() {
+    name = document.getElementById("name-id").value;
+     if (chitai_gorod_win) chitai_gorod_win.close();
+    chitai_gorod_win = window.open(`https://www.chitai-gorod.ru/search?phrase=${name}`);
+}
+
+function findOnOzon() {
+    name = document.getElementById("name-id").value;
+     if (ozon_win) ozon_win.close();
+    ozon_win = window.open(`https://www.ozon.ru/category/knigi-16500/?text=${name.replaceAll(' ','+')}`);
+}
+
 function getBookByISBN() {
     if (isbn_list.length) {
         if (isbn_index<isbn_list.length) {
-            document.getElementById("isbn-id").value = isbn_list[isbn_index];
+            document.getElementById("isbn-id").value = isbn_list[isbn_index].split(':')[0];
+            pic1 = isbn_list[isbn_index].split(':')[1]
+            pic2 = isbn_list[isbn_index].split(':')[2]
             isbn_index += 1;
         } else {
             document.getElementById("isbn-count").innerHTML = 'Все книги из файла обработаны';
